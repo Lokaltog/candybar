6f82094a943db5f7238ad45757217a30300ae63d
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-02-25 20:24:03 +0100

    Move ewmh property functions to desktops.c

---
+++ src/widgets/desktops.h
@@ -0,0 +1,2 @@
+static bool ewmh_get_active_window_name(xcb_ewmh_connection_t *ewmh, int screen_nbr, char *window_name);
+static bool ewmh_get_desktop_list(xcb_ewmh_connection_t *ewmh, int screen_nbr, desktop_t *desktops);
--- src/wkline.h
+++ src/wkline.h
@@ -38,8 +38,6 @@ typedef struct thread_data_t {
 	ewmh_data_t *ewmh;
 } thread_data_t;
 
-static bool ewmh_get_active_window_name(xcb_ewmh_connection_t *ewmh, int screen_nbr, char *window_name);
-static bool ewmh_get_desktop_list(xcb_ewmh_connection_t *ewmh, int screen_nbr, desktop_t *desktops);
 static gboolean wk_web_view_inject (char *payload);
 static gboolean wk_context_menu_cb (WebKitWebView *web_view, GtkWidget *window);
 static void wk_notify_load_status_cb (WebKitWebView *web_view, GParamSpec *pspec, GtkWidget *window);
