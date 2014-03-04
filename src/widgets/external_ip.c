a54703994f7ce010277c3e67808250b747787407
Ricardo Vieira <ricardo.vieira@tecnico.ulisboa.pt>
2014-03-04 19:30:05 +0000

    config: get initial support for a json config file

--- src/widgets/external_ip.c
+++ src/widgets/external_ip.c
@@ -1,13 +1,15 @@
 #include "widgets.h"
 #include "external_ip.h"
+#include "util/load_config.h"
 
 static int
-widget_external_ip_send_update () {
-	char *external_ip = wkline_curl_request(wkline_widget_external_ip_address);
-
-	json_t *json_data_object = json_object();
+widget_external_ip_send_update (struct wkline_widget_t *widget) {
 	char *json_payload;
+	char *external_ip;
+	json_t *json_data_object;
 
+	external_ip = wkline_curl_request(json_string_value(wkline_widget_get_config(widget, "address")));
+	json_data_object = json_object();
 	json_object_set_new(json_data_object, "ip", json_string(external_ip));
 
 	json_payload = json_dumps(json_data_object, 0);
@@ -23,9 +25,9 @@ widget_external_ip_send_update () {
 }
 
 void
-*widget_external_ip () {
+*widget_external_ip (struct wkline_widget_t *widget) {
 	for (;;) {
-		widget_external_ip_send_update();
+		widget_external_ip_send_update(widget);
 
 		sleep(600);
 	}
