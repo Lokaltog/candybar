228ed2a3b941987757338f19e0508444a3da7e3f
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-03-05 10:57:06 +0100

    widgets: handle widget disable flags from autotools

--- src/widgets/widgets.h
+++ src/widgets/widgets.h
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <webkit/webkit.h>
 #include <xcb/xcb_ewmh.h>
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 struct widget {
 	const char *name;
@@ -27,14 +30,30 @@ gboolean update_widget (struct widget *widget);
 void window_object_cleared_cb (WebKitWebView *web_view, GParamSpec *pspec, gpointer context, gpointer window_object, gpointer user_data);
 
 static const struct widget_call wkline_widgets[] = {
-	{.func=widget_battery,         .name="battery"},
-	{.func=widget_desktops,        .name="desktops"},
-	{.func=widget_external_ip,     .name="external_ip"},
-	{.func=widget_notifications,   .name="notifications"},
+#ifndef DISABLE_WIDGET_BATTERY
+	{.func=widget_battery, .name="battery"},
+#endif
+#ifndef DISABLE_WIDGET_DESKTOPS
+	{.func=widget_desktops, .name="desktops"},
+#endif
+#ifndef DISABLE_WIDGET_EXTERNAL_IP
+	{.func=widget_external_ip, .name="external_ip"},
+#endif
+#ifndef DISABLE_WIDGET_NOTIFICATIONS
+	{.func=widget_notifications, .name="notifications"},
+#endif
+#ifndef DISABLE_WIDGET_NOW_PLAYING_MPD
 	{.func=widget_now_playing_mpd, .name="now_playing_mpd"},
-	{.func=widget_volume,          .name="volume"},
-	{.func=widget_weather,         .name="weather"},
-	{.func=widget_window_title,    .name="window_title"},
+#endif
+#ifndef DISABLE_WIDGET_VOLUME
+	{.func=widget_volume, .name="volume"},
+#endif
+#ifndef DISABLE_WIDGET_WEATHER
+	{.func=widget_weather, .name="weather"},
+#endif
+#ifndef DISABLE_WIDGET_WINDOW_TITLE
+	{.func=widget_window_title, .name="window_title"},
+#endif
 };
 
 #define MISSING_VALUE ""
