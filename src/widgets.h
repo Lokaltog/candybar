744700a46b5ccddd580024a3c7c798db4d2adb33
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-03-08 08:06:53 +0100

    widgets: remove widget declarations from widgets.h

--- src/widgets.h
+++ src/widgets.h
@@ -15,43 +15,10 @@ struct widget_call {
 	const char *name;
 };
 
-void* widget_battery ();
-void* widget_desktops ();
-void* widget_external_ip ();
-void* widget_notifications ();
-void* widget_now_playing_mpd ();
-void* widget_volume ();
-void* widget_weather ();
-void* widget_window_title ();
 gboolean update_widget (struct widget *widget);
 void window_object_cleared_cb (WebKitWebView *web_view, GParamSpec *pspec, gpointer context, gpointer window_object, gpointer user_data);
 
-static const struct widget_call wkline_widgets[] = {
-#ifndef DISABLE_WIDGET_BATTERY
-	{ .func = widget_battery, .name = "battery" },
-#endif
-#ifndef DISABLE_WIDGET_DESKTOPS
-	{ .func = widget_desktops, .name = "desktops" },
-#endif
-#ifndef DISABLE_WIDGET_EXTERNAL_IP
-	{ .func = widget_external_ip, .name = "external_ip" },
-#endif
-#ifndef DISABLE_WIDGET_NOTIFICATIONS
-	{ .func = widget_notifications, .name = "notifications" },
-#endif
-#ifndef DISABLE_WIDGET_NOW_PLAYING_MPD
-	{ .func = widget_now_playing_mpd, .name = "now_playing_mpd" },
-#endif
-#ifndef DISABLE_WIDGET_VOLUME
-	{ .func = widget_volume, .name = "volume" },
-#endif
-#ifndef DISABLE_WIDGET_WEATHER
-	{ .func = widget_weather, .name = "weather" },
-#endif
-#ifndef DISABLE_WIDGET_WINDOW_TITLE
-	{ .func = widget_window_title, .name = "window_title" },
-#endif
-};
+static const struct widget_call wkline_widgets[] = { };
 
 #define MISSING_VALUE ""
 #define LENGTH(X) (sizeof X / sizeof X[0])
