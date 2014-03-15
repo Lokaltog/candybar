3ede81bdf16a3c17131026f39b11bfd6a3211a1e
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-03-15 13:47:48 +0100

    widgets/magick: use height from config

--- src/widgets/magick_background.h
+++ src/widgets/magick_background.h
@@ -5,13 +5,11 @@ static struct widget_config {
 	const char *css_gradient_overlay;
 	int blur_radius;
 	int brightness;
-	int height;
 	int saturation;
 } widget_config_defaults = {
 	.image = "",
 	.css_gradient_overlay = "top, transparent, rgba(0, 0, 0, .5)",
 	.blur_radius = 0,
 	.brightness = 100,
-	.height = 40,
 	.saturation = 100,
 };
