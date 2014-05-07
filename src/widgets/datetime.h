4ccba46808dc68fe87764803505734e4c65d997f
Kim Silkebækken <kim@silkebaekken.net>
2014-05-07 15:07:57 +0200

    widgets/datetime: remove seconds from default format

--- src/widgets/datetime.h
+++ src/widgets/datetime.h
@@ -4,6 +4,6 @@ static struct widget_config {
 	int refresh_interval;
 } widget_config_defaults = {
 	.date_format = "%Y-%m-%d",
-	.time_format = "%H:%M:%S",
-	.refresh_interval = 1,
+	.time_format = "%H:%M",
+	.refresh_interval = 10,
 };
