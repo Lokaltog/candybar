80f8205d8138f35a071d350437cb8f7d32ad9588
Kim Silkebækken <kim@silkebaekken.net>
2014-05-09 08:21:17 +0200

    widgets/mpd: remove unused update_interval config option

--- src/widgets/now_playing_mpd.h
+++ src/widgets/now_playing_mpd.h
@@ -4,10 +4,8 @@ static struct widget_config {
 	const char *host;
 	int port;
 	int timeout;
-	int update_interval;
 } widget_config_defaults = {
 	.host = "localhost",
 	.port = 6600,
 	.timeout = 5000,
-	.update_interval = 1000,
 };
